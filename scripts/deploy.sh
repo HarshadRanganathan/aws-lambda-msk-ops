34d3b271ed34dee548e4d00d56b4f5422402eaf3
Harshad Ranganathan <32612787+HarshadRanganathan@users.noreply.github.com>
2024-02-12 12:32:00 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 # Publish the image to ECR registry
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin xxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com
 
-docker tag kafka-ops-lambda xxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com/kafka-ops-lambda:latest
+docker tag msk-ops-lambda xxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com/msk-ops-lambda:latest
 
-docker push xxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com/kafka-ops-lambda:latest
+docker push xxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com/msk-ops-lambda:latest
